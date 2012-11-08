3929b92289c9fb42688fa984ca069ca0977b6e38
austin dreyer <dreyerau@gmail.com>
2012-11-08 17:16:19 -0500

    add reproduction fuction

--- New allometry modeling script nov 1.R
+++ New allometry modeling script nov 1.R
@@ -35,14 +35,57 @@ alleles<-function(s.x,s.sd,i1.x,i2.x,i1.sd,i2.sd,k1.x,k2.x,k1.sd,k2.sd)
 	return(alle)
 }
 
-## function to create intial population
+## function to create intial population. 
 
-start.pop<-function()
+start.pop<-function(N,s.x,s.sd,i1.x,i2.x,i1.sd,i2.sd,k1.x,k2.x,k1.sd,k2.sd,h)
 {
+	#use function alleles() to create a set of alleles for popn
+	all<-alleles(s.x,s.sd,i1.x,i2.x,i1.sd,i2.sd,k1.x,k2.x,k1.sd,k2.sd)
 	
+	#generate common systemic factor 's'
+	sa<-sample(all$sA,N,replace=T)
+ 	sb<-sample(all$sB,N,replace=T)
+ 	#the actual parameter 's' includes an input to control for environmental vs. genetic input, h.  simulating heritability 
+ 	s<-((sa+sb)*h)+(rnorm(N,s.x,s.sd)*(1-h))
+ 	
+ 	#generate T1 specific components
+	k1a<-sample(all$k1A,N,replace=T)
+ 	k1b<-sample(all$k1B,N,replace=T)
+ 	k1<-(k1a+k1b)
+ 
+ 	i1a<-sample(all$i1A,N,replace=T)
+ 	i1b<-sample(all$i1B,N,replace=T)
+ 	i1<-(i1a+i1b)
+
+ 	b1<-1
+ 	
+ 	#create T1
+ 	T1<-b1*exp(s*k1+i1)
+ 	
+ 	#generate T2 specific components
+ 	k2a<-sample(all$k2A,N,replace=T)
+ 	k2b<-sample(all$k2B,N,replace=T)
+ 	k2<-(k2a+k2b)
+ 
+ 	i2a<-sample(all$i2A,N,replace=T)
+ 	i2b<-sample(all$i2B,N,replace=T)
+ 	i2<-(i2a+i2b)
+ 	
+ 	b2<-1
+ 	
+ 	#create T2
+ 	T2<-b2*exp(s*k2+i2)
+ 	
+ 	#compile population
+ 	d<-data.frame(T1,T2,sa,sb,s,i1a,i1b,i1,i2a,i2b,i2,k1a,k1b,k1,k2a,k2b,k2)
+ 	d$lT1<-log(d$T1)
+ 	d$lT2<-log(d$T2)
+ 	
+ 	return(d)
 }
 
 ## function to reproduce
+
 
 
 ## selection functions (stabilizing, directional, correlational, proportional)
